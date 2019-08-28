306f293546e469ffc65023eb2b8b7ca1343f9044
Timeron <timeron@gmail.com>
2019-08-28 14:49:51 +0200

    WALL2-3
    Logowanie ze SpringBoota

--- src/main/java/walletservice/controller/MainRunController.java
+++ src/main/java/walletservice/controller/MainRunController.java
@@ -2,14 +2,19 @@ package walletservice.controller;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainRunController {
 
+	Logger logger = LoggerFactory.getLogger(MainRunController.class);
+	
 	@RequestMapping("/service")
 	public String angularRun(Map<String, Object> model) {
+		logger.info("Open: Service Angular Page");
 		return "angular";
 	}
 }
--- src/main/java/walletservice/controller/WelcomeController.java
+++ src/main/java/walletservice/controller/WelcomeController.java
@@ -2,6 +2,8 @@ package walletservice.controller;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,11 @@ public class WelcomeController {
 	@Autowired
 	private WalletProperties properties;
 
+	Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+	
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
+		logger.info("Open: Welcome Page");
 		model.put("message", this.properties.getWelcomeMessage());
 		model.put("prop", this.properties.getEnv());
 		return "welcome";
